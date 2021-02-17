4f575e9574926a0b598ac56916471c92ab6fa9f7
Adnan Reza <adnan.reza@outlook.com>
2021-02-17 14:05:08 -0800

    Emit messages on enter and leaving chatroom

--- server.js
+++ server.js
@@ -12,8 +12,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when client connects
 io.on('connection', socket => {
-  console.log('New WS connection...');
+  // Welcome current user
   socket.emit('message', 'Welcome to LetsChat!')
+
+  // Broadcast when a user connects
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  // Runs when client diconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  })
 })
 
 const PORT = process.env.PORT || 3000;
