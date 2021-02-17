5cad04625e1131a305442494c0a3523a6e246d7c
Adnan Reza <adnan.reza@outlook.com>
2021-02-17 14:19:47 -0800

    Added event listener for chatForm submit button

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,15 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
 
 socket.on('message', msg => {
   console.log(msg)
+})
+
+// Message submitted
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const msgE = e.target.elements.msgE.value;
+
+  console.log(msgE)
 })
