4fa7e947e0170f25fa6206f928ffff44deac8519
Adnan Reza <adnan.reza@outlook.com>
2021-02-17 17:58:04 -0800

    cleared input and focused on input on enter

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,14 @@
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages')
 
 const socket = io();
 
 socket.on('message', msg => {
   console.log(msg)
   outputMessage(msg);
+
+  //Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 })
 
 // Message submitted
@@ -16,6 +20,10 @@ chatForm.addEventListener('submit', (e) => {
 
   // Emit message to server
   socket.emit('chatMessage', msgE)
+
+  // Clear input
+  e.target.elements.msgE.value = '';
+  e.target.elements.msgE.focus();
 })
 
 // Output message to DOM
