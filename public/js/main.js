8ead0a6ed186761e66e0982b45038e5bf27d517c
Adnan Reza <adnan.reza@outlook.com>
2021-02-17 17:01:17 -0800

    Added outputMessage function

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,27 @@ const socket = io();
 
 socket.on('message', msg => {
   console.log(msg)
+  outputMessage(msg);
 })
 
 // Message submitted
 chatForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  // Get message text
   const msgE = e.target.elements.msgE.value;
 
-  console.log(msgE)
+  // Emit message to server
+  socket.emit('chatMessage', msgE)
 })
+
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+  <p class="text">
+    ${message}
+  </p>`;
+  document.querySelector('.chat-messages').appendChild(div);
+}
